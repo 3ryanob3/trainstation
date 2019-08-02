7bb6ea036d824749fca72f8a97f2a3952608af4b
Iby <Eryanobe@me.com>
2019-08-01 18:33:48 -0700

    updated but still isnt working

--- assets/javacript.js
+++ assets/javacript.js
@@ -1,74 +1,100 @@
 $(document).ready(function () {
     // Initialize Firebase
-    var config = {
-        apiKey: "AIzaSyAeN4Ugc9K2Jdr44YhuL1HE0naqyicvBSA",
-        authDomain: "erya-833f6.firebaseapp.com",
-        databaseURL: "https://erya-833f6.firebaseio.com",
-        projectId: "erya-833f6",
-        storageBucket: "erya-833f6.appspot.com",
-        messagingSenderId: "843649133939"
-    };  
-    firebase.initializeApp(config);
+    
+    
+    var firebaseConfig = {
+        apiKey: "AIzaSyD3VUf8U5hiucMgLk40HBctNKmVYI0RB0M",
+        authDomain: "train-d6dca.firebaseapp.com",
+        databaseURL: "https://train-d6dca.firebaseio.com",
+        projectId: "train-d6dca",
+        storageBucket: "",
+        messagingSenderId: "637292608712",
+        appId: "1:637292608712:web:0209cc4878f76b42"
+      }; 
+      firebase.initializeApp(firebaseConfig);
     
     
    
 
     var database = firebase.database();
 
-    //  Button for adding Employees
+    //  add train
     $("#add-train-btn").on("click", function (event) {
         event.preventDefault();
 
-        var firstTime = $("#start-input").val().trim();
-
+        
         // Grabs user input
         var trainName = $("#train-input").val().trim();
         var trainDes = $("#destination-input").val().trim();
-        var trainStart = moment().format(firstTime);
-        console.log(trainStart)
+        var trainFrequency = $("#frequency-input").val().trim();
+        var trainTime = $("#time-input").val().trim();
+        
+        console.log(trainTime)
 
-        // Creates local "temporary" object for holding employee data
+        // Creates Object for train data 
         var newTrain = {
             name: trainName,
             destination: trainDes,
-            start: trainStart,
+            frequency: trainFrequency,
+            start: trainTime,
 
         };
 
-        // Uploads employee data to the database
+        // Push train data to data base
         database.ref().push(newTrain);
 
-        // Alert
-        // alert("Employee successfully added");
+        
+        
 
         // Clears all of the text-boxes
         $("#train-input").val("");
         $("#destination-input").val("");
-        $("#start-input").val("");
+        $("#frequency-input").val("");
+        $("#time-input").val("");
 
+    });
+        //  Create Firebase event 
+        database.ref().on("child_added", function (childSnapshot) {
 
-        //  Create Firebase event for adding employee to the database and a row in the html when a user adds an entry
-        database.ref().on("child_added", function (childSnapshot, prevChildKey) {
-
-            // console.log(childSnapshot.val());
+         
 
             // Store everything into a variable.
             var trainName = childSnapshot.val().name;
             var trainDes = childSnapshot.val().destination;
-            var trainStart = childSnapshot.val().start;
+            var trainFrequency = childSnapshot.val().frequency;
+            var trainTime = childSnapshot.val().time;
 
 
 
-            // var trainStartMoment = moment.unix(trainStart).format("HH:mm");
-            var trainStartMoment = moment().format("HH:mm");
-
-            $("#employee-table > tbody").append("<tr><td>" + trainName + "</td><td>" + trainDes + 'empMonths' + "</td><td>" + 'math' + "</td><td>" + 'empBilled' + "</td></tr>");
-        });
-
-        var now = moment().format("HH:mm");
-        
+          // use moment to get tran times  
+        var trainTimeConverter = moment(trainTime, "hh:mm").subtract(1, "years");
+        console.log(trainTimeConverter);
         
 
 
 
+        var diffTime = moment().diff(moment(trainTimeConverter), "minutes");
+        var timeApart = diffTime % trainFrequency;
+        var minutesArrival = trainFrequency - timeApart;
+        var timeArrival = moment().add(minutesArrival, "m").form("LT");
 
+        //Create new row
+        var newRow = $("<tr>").append(
+        $("<td>").text(trainName),
+        $("<td>").text(trainDestinantion),
+        $("<td>").text(trainFrequency + " min"),
+        $("<td>").text(minutesArrival + " min"),
+        $("<td>").text(timeArrival)
+        
+        
+        );
+
+        
+        //Here append new row to the table
+        $("#schedule-table > tbody").append(newRow);
+
+    });
+});
+
+
+    
